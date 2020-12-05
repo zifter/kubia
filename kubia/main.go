a20db9e3a7d1cbf0b4972b37374431392d95e8ef
Oleg Strokachuk <oleg.strokachuk@vizor-games.com>
2020-12-06 00:58:45 +0300

    second and third title passed

--- kubia/main.go
+++ kubia/main.go
@@ -8,11 +8,13 @@ import (
 )
 
 func main() {
+	hostname, _ := os.Hostname()
+
+	log.Printf("Server started on ... %s", hostname)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Print("Received request from " + r.RemoteAddr)
 
-		name, _ := os.Hostname()
-		fmt.Fprintf(w, "You've hit %v\n", name)
+		fmt.Fprintf(w, "You've hit %v\n", hostname)
 	})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Panic(err)
